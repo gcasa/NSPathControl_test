d582ec02a64e36df7956da266be079ce17b39d61
Gregory Casamento <greg.casamento@gmail.com>
2020-04-22 18:06:02 -0400

    Initial Commit

---
+++ NSPathControl_test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSPathControl_test
+//
+//  Created by Gregory Casamento on 4/22/20.
+//  Copyright © 2020 Open Logic Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
